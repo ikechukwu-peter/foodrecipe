72e7aa1a4a182fc6cd121ab84bc7fd7c54207dd2
Peter <pitiyke@gmail.com>
2020-08-09 13:05:05 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Recipe from './Recipe';
+import Footer from './Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+ const App = () => {
+    const APP_ID = "fb99b9f8";
+    const APP_KEY = "9e84fea944801b6cd457fc33f99ccdf2	";
+    const [search, setSearch] = useState("");
+    const [recipes, setRecipes] = useState([]);
+    const [query, setQuery] = useState("chicken");
+
+
+          useEffect(() => {
+            getRecipes();
+            
+          }, [query]);
+          
+
+          const getRecipes = async () => {
+            const response = await fetch (`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+            const data = await response.json();
+            setRecipes(data.hits)
+            };
+
+            const updateSearch = e => {
+              setSearch(e.target.value);
+            };
+
+            const getSearch = e => {
+              e.preventDefault();
+              setQuery(search);
+              setSearch('')
+            };
+
+
+            return (
+              <div className="App">
+             
+                <header className="header"> Food Recipes</header>
+                
+               
+                <form onSubmit={getSearch} className="search-form">
+                  <input className="search-bar" type="text" value={search} 
+                  onChange={updateSearch}/>
+                  <button className="search-button" type="submit">Search</button>
+                </form>
+                <div className="recipe-container">
+                <div className="recipes">
+              {recipes.map(recipe =>(
+                  <Recipe 
+                  key={recipe.recipe.label}
+                  title={recipe.recipe.label}
+                  calories={recipe.recipe.calories}
+                  image={recipe.recipe.image}
+                  ingredients = {recipe.recipe.ingredients}
+                  />
+                ))}
+              </div>
+                </div>
+            
+              
+              <Footer/>
+              </div>
+            );
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer className="footer">
+            &#169;Food Recipes 2020 by Ikechukwu Peter
+              </footer>
+        </div>
+    )
+}
+
+export default Footer;
